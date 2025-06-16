15996192b5b670da9c97f1b076513d1ad59ad2cf
Saurav-Rana-Ekodemy <saurav@ekodemy.in>
2025-06-16 10:03:01 +0530

    First Commit

---
+++ android/app/src/main/java/com/intellio/intellio/MainActivity.java
@@ -0,0 +1,6 @@
+package com.intellio.intellio;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+
+import '../modules/feed/bindings/feed_binding.dart';
+import '../modules/feed/views/feed_view.dart';
+import '../modules/login/bindings/login_binding.dart';
+import '../modules/login/views/login_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.LOGIN;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.FEED,
+      page: () => const FeedView(),
+      binding: FeedBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
+    ),
+  ];
+}
