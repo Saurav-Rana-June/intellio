7dd0596a2082bc289757524175ba434e17f263f5
Saurav-Rana-June <sauravsevenjune@gmail.com>
2025-07-09 10:15:47 +0530

    Firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:Intellio/infrastructure/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async  {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     GetMaterialApp(
       title: "Intellio",
